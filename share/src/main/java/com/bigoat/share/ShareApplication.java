b3201896932a4b5f54b7de122905427c4b1043f8
bigoat <bigoatsm@gmail.com>
2019-05-27 23:56:16 +0800

    优化代码

---
+++ share/src/main/java/com/bigoat/share/ShareApplication.java
@@ -0,0 +1,12 @@
+package com.bigoat.share;
+
+import com.bigoat.base.BaseApplication;
+import com.bigoat.base.Config;
+
+public class ShareApplication extends BaseApplication {
+
+    @Override
+    public void myCreate(Config config) {
+
+    }
+}
